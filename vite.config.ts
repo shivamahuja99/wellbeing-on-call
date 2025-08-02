e75bd3f946a02822f3b336d0eb59e8a5ec77fefe
skullcandy69 <shiv.ahuja1494@gmail.com>
2025-08-03 02:29:11 +0530

    vite config updated with repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/wellbeing-on-call/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {

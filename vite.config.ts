d417ae8ea2425c9fc94db104c8e88f9cf966dc50
Subhanshu <subhanshu@aampe.com>
2025-08-04 01:43:18 +0530

    Fix gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,23 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => {
-  console.log(mode);
   return {
-    base: mode === 'production' ? '/wellbeing-on-call/' : '/',
+    base: '/wellbeing-on-call/',
     server: {
       host: "::",
       port: 8080,
     },
     plugins: [
       react(),
-    ].filter(Boolean),
+    ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
       },
+    },
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
     },
   }
 });

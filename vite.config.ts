6da253b874373af29803f595fee9d018b0c28605
Subhanshu <subhanshu@aampe.com>
2025-08-03 14:36:14 +0530

    minor

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  base: '/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  console.log(mode);
+  return {
+    base: mode === 'production' ? '/wellbeing-on-call/' : '/',
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  }
+});
